45f0b6c325c45a9dc5361db63da084809e85b0ef
BytesDex <bytesdex.gt@gmail.com>
2024-07-05 20:27:17 -0600

    Update script-62.js

--- resources/js/script-62.js
+++ resources/js/script-62.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
         "imagen1": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9sb2dvLnBuZw",
         "imagen2": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9iYWJ5IHBsYXllciAtIHdlYmNvZGVtLnBuZw",
         "imagen3": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi90ZXh0dXJhLnBuZw",
-        "imagen4": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9ieXRlc2RleC1tYXJrZXRwbGFjZS5wbmc"
+        "imagen4": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9ieXRlc2RleC1tYXJrZXRwbGFjZS5wbmc",
+        "imagen5": "aHR0cDovL2NkbS1ieXRlc2RleC5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2ZhbGxvLWRlLXNpc3RlbWEucG5n"
     };
 
     for (const [id, url] of Object.entries(contentUrls)) {
