a354dce1594f49570b7488340a5116bfb68da5d8
BytesDex <bytesdex.gt@gmail.com>
2024-10-17 22:33:03 -0600

    Update script-15.js

--- resources/js/script-15.js
+++ resources/js/script-15.js
@@ -3,10 +3,6 @@
             menu.classList.toggle('show');
         }
 
-        function redirectToPage(page) {
-            window.location.href = page;
-        }
-
         window.onclick = function (event) {
             const menu = document.getElementById('dropdown-menu');
             if (event.target !== menu && event.target !== document.querySelector('.menu-icon')) {
