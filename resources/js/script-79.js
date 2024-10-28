77e2655c4c2a472284edb2df2dce575d9e4034c3
BytesDex <bytesdex.gt@gmail.com>
2024-10-28 03:29:29 -0600

    Update script-79.js

--- resources/js/script-79.js
+++ resources/js/script-79.js
@@ -1,13 +1,13 @@
 const codes = {
     code1: 'SDRLN1I5',
     code2: 'QTdCM0M5',
-    code3: 'Y29kaW8z'
+    code3: 'WDJZOFo1'
 };
 
 const downloadLinks = {
     'SDRLN1I5': 'aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xRXprbnZ2M3lBOEk1b3Q5U1c4d1FWQ2Fwbmc0ckFIUXkvdmlldw',
     'QTdCM0M5': 'aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xSDg5MVNuMHNrcm5XZVotcHFCOG52UUt3N1ZwUm4xT2Ivdmlldz91c3A9ZHJpdmVzZGs',
-    'Y29kaW8z': 'aHR0cHM6Ly9kb2N1bWVudC5jb20vZG93bmxvYWQvcGF0aC90ZXN0My5qcGc='
+    'WDJZOFo1': 'aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xSEo2elFBNGFrV1hWV2l0ZTRmMGJzblJHZ1ViVU5LeVQvdmlldz91c3A9ZHJpdmVzZGs'
 };
 
 function handleKeyPress(event) {
@@ -29,7 +29,7 @@ function validateCode(userCode) {
         window.location.href = atob(downloadLinks['QTdCM0M5']);
     } else if (userCode === atob(codes.code3)) {
         inputField.classList.add('correct');
-        window.location.href = atob(downloadLinks['Y29kaW8z']);
+        window.location.href = atob(downloadLinks['WDJZOFo1']);
     } else {
         inputField.classList.add('error');
         alert("Código incorrecto. Por favor, intenta de nuevo.");
