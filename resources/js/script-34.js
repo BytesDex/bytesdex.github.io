9ffc7432324cc206010399840c1b9f0d4acd9084
BytesDex <bytesdex.gt@gmail.com>
2024-11-16 20:34:50 -0600

    Update script-34.js

--- resources/js/script-34.js
+++ resources/js/script-34.js
@@ -72,6 +72,10 @@ function displayUserData(username, profilePic) {
   const userContainer = document.getElementById('userContainer');
   userContainer.innerHTML = '';
 
+  const userInfoContainer = document.createElement('div');
+  userInfoContainer.style.display = 'flex';
+  userInfoContainer.style.alignItems = 'center';
+
   const img = document.createElement('img');
   img.src = profilePic;
   img.alt = 'Foto de perfil';
@@ -83,9 +87,28 @@ function displayUserData(username, profilePic) {
 
   const span = document.createElement('span');
   span.textContent = username;
+  span.style.display = 'flex';
+  span.style.alignItems = 'center';
 
-  userContainer.appendChild(img);
-  userContainer.appendChild(span);
+  if (username === "Bytes") {
+    const verifiedIcon = document.createElement('span');
+    verifiedIcon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+        <g fill="none" stroke-linejoin="round" stroke-width="4">
+          <path fill="#2f88ff" stroke="#rgba(0, 0, 0, 0.5)" d="M24 44C29.5228 44 34.5228 41.7614 38.1421 38.1421C41.7614 34.5228 44 29.5228 44 24C44 18.4772 41.7614 13.4772 38.1421 9.85786C34.5228 6.23858 29.5228 4 24 4C18.4772 4 13.4772 6.23858 9.85786 9.85786C6.23858 13.4772 4 18.4772 4 24C4 29.5228 6.23858 34.5228 9.85786 38.1421C13.4772 41.7614 18.4772 44 24 44Z" />
+          <path stroke="#fff" stroke-linecap="round" d="M16 24L22 30L34 18" />
+        </g>
+      </svg>`;
+    verifiedIcon.style.marginLeft = '5px';
+    verifiedIcon.style.marginTop = '6px';
+    verifiedIcon.style.marginBottom = '0';
+    span.appendChild(verifiedIcon);
+  }
+
+  userInfoContainer.appendChild(img);
+  userInfoContainer.appendChild(span);
+
+  userContainer.appendChild(userInfoContainer);
 
   img.onclick = span.onclick = chooseProfilePicture;
-}
+  }
