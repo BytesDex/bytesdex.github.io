9bbf18dc5e0eb9b1e0dab2bfeb0c32f4f2710d86
BytesDex <bytesdex.gt@gmail.com>
2024-11-17 04:00:03 -0600

    Create script-81.js

---
+++ resources/js/script-81.js
@@ -0,0 +1,21 @@
+const verifiedUsers = ["Bytes"];  
+
+function checkVerification() {
+  const currentUser = localStorage.getItem("currentUser");
+
+  if (verifiedUsers.includes(currentUser)) {
+    window.location.href = "#"; 
+  } else {
+    showAccessDeniedAlert();
+  }
+}
+
+function showAccessDeniedAlert() {
+  const alertBox = document.getElementById("access-denied-alert");
+  alertBox.style.display = "block"; 
+}
+
+function closeAlert() {
+  const alertBox = document.getElementById("access-denied-alert");
+  alertBox.style.display = "none"; 
+}
