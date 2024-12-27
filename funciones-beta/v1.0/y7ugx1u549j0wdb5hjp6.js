e0967d2c675847ac405eb2e99186cd4869d2ddc6
BytesDex <bytesdex.gt@gmail.com>
2024-12-27 05:02:11 -0600

    Update y7ugx1u549j0wdb5hjp6.js

--- funciones-beta/v1.0/y7ugx1u549j0wdb5hjp6.js
+++ funciones-beta/v1.0/y7ugx1u549j0wdb5hjp6.js
@@ -4407,7 +4407,7 @@ else if (mensajeLower.includes('no')) {
     divMensaje.innerHTML = `
         ${esUsuario ? '' : `
             <div class="flex items-center">
-                <img src="https://cdm-bytesdex.github.io/resources/multimedia/imagen/bot.jpg" alt="Avatar IA" class="w-10 h-10 rounded-full mr-3 border-2 border-gray-500">
+                <img src="https://cdn-bytesdex.github.io/resources/multimedia/imagen/bot.jpg" alt="Avatar IA" class="w-10 h-10 rounded-full mr-3 border-2 border-gray-500">
             </div>
         `}
         <div class="${esUsuario ? 'ml-auto bg-blue-500 text-white' : 'bg-white'} mensaje-burbuja">
@@ -4452,7 +4452,7 @@ mostrarIndicadorEscritura() {
 
     divMensaje.innerHTML = `
         <div class="flex items-center">
-            <img src="https://cdm-bytesdex.github.io/resources/multimedia/imagen/bot.jpg" alt="Avatar IA" class="w-10 h-10 rounded-full mr-3 border-2 border-gray-500">
+            <img src="https://cdn-bytesdex.github.io/resources/multimedia/imagen/bot.jpg" alt="Avatar IA" class="w-10 h-10 rounded-full mr-3 border-2 border-gray-500">
             <div class="typing-bubble">
                 <div class="dot"></div>
                 <div class="dot"></div>
