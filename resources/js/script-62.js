13df38375d9c4a6d60df98c1f2f783406c3f5f53
BytesDex <bytesdex.gt@gmail.com>
2024-12-27 19:50:11 -0600

    Update script-62.js

--- resources/js/script-62.js
+++ resources/js/script-62.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const contentUrls = {
-        "imagen1": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9iYWJ5IHBsYXllciAtIGJ5dGVzZGV4LnBuZw",
+        "imagen1": "aHR0cHM6Ly9jZG4tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9iYWJ5IHBsYXllciAtIGJ5dGVzZGV4LnBuZw",
         "imagen2": "aHR0cHM6Ly9jZG4tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi90ZXh0dXJhLnBuZw",
         "imagen3": "aHR0cHM6Ly9jZG4tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9ieXRlc2RleC1tYXJrZXRwbGFjZS5wbmc",
         "imagen4": "aHR0cDovL2Nkbi1ieXRlc2RleC5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2ZhbGxvLWRlLXNpc3RlbWEucG5n"
