28556b0b58217a7235db719356be21a556c83106
BytesDex <bytesdex.gt@gmail.com>
2024-12-27 19:17:07 -0600

    Update script-62.js

--- resources/js/script-62.js
+++ resources/js/script-62.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const contentUrls = {
-        "imagen1": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9sb2dvLnBuZw",
-        "imagen2": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9iYWJ5IHBsYXllciAtIHdlYmNvZGVtLnBuZw",
-        "imagen3": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi90ZXh0dXJhLnBuZw",
-        "imagen4": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9ieXRlc2RleC1tYXJrZXRwbGFjZS5wbmc",
-        "imagen5": "aHR0cDovL2NkbS1ieXRlc2RleC5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2ZhbGxvLWRlLXNpc3RlbWEucG5n"
+        "imagen1": "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9iYWJ5IHBsYXllciAtIGJ5dGVzZGV4LnBuZw",
+        "imagen2": "aHR0cHM6Ly9jZG4tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi90ZXh0dXJhLnBuZw",
+        "imagen3": "aHR0cHM6Ly9jZG4tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9ieXRlc2RleC1tYXJrZXRwbGFjZS5wbmc",
+        "imagen4": "aHR0cDovL2Nkbi1ieXRlc2RleC5naXRodWIuaW8vcmVzb3VyY2VzL211bHRpbWVkaWEvaW1hZ2VuL2ZhbGxvLWRlLXNpc3RlbWEucG5n"
     };
 
     for (const [id, url] of Object.entries(contentUrls)) {
@@ -15,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    const faviconUrl = "aHR0cHM6Ly9jZG0tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9sb2dvLnBuZw";
+    const faviconUrl = "aHR0cHM6Ly9jZG4tYnl0ZXNkZXguZ2l0aHViLmlvL3Jlc291cmNlcy9tdWx0aW1lZGlhL2ltYWdlbi9ieXRlc2RleC1sb2dvLnBuZw";
     const decodedFaviconUrl = atob(faviconUrl);
     const linkElement = document.createElement('link');
     linkElement.rel = 'icon';
