07e4d43aad3a688516ef5fc20dfaa867cdb66990
BytesDex <bytesdex.gt@gmail.com>
2024-12-29 03:16:57 -0600

    Update script-83.js

--- resources/js/script-83.js
+++ resources/js/script-83.js
@@ -29,7 +29,7 @@ function displayUserData(username, profilePic) {
     img.style.height = '100px';
     img.style.marginBottom = '10px';
     img.style.cursor = 'pointer';
-    img.style.boxShadow = '0 0 25px 10px rgba(0, 0, 0, 0.8)'; 
+    img.style.boxShadow = '0 0 15px 5px rgba(0, 0, 0, 0.5)'; 
     
     const span = document.createElement('span');
     span.textContent = username;
