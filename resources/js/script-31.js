3697f755fc72a1bcc9af14d6df2e449f7a6f7f32
BytesDex <bytesdex.gt@gmail.com>
2025-01-02 01:43:27 -0600

    Update script-31.js

--- resources/js/script-31.js
+++ resources/js/script-31.js
@@ -2,56 +2,67 @@ const checkboxTerms = document.getElementById('terms-checkbox');
 const acceptButtonTerms = document.getElementById('accept-button-terms');
 const thankYouMessageTerms = document.getElementById('thank-you-message-terms');
 
-if (localStorage.getItem('acceptedTerms')) {
+if (thankYouMessageTerms && localStorage.getItem('acceptedTerms')) {
     document.querySelector('.terms-section').style.display = 'none';  
     thankYouMessageTerms.style.display = 'block';  
 } else {
-    checkboxTerms.addEventListener('change', function() {
-        acceptButtonTerms.disabled = !this.checked;
-    });
+    if (checkboxTerms) {
+        checkboxTerms.addEventListener('change', function() {
+            acceptButtonTerms.disabled = !this.checked;
+        });
+    }
 
-    acceptButtonTerms.addEventListener('click', function() {
-        localStorage.setItem('acceptedTerms', 'true');  
-        document.querySelector('.terms-section').style.display = 'none';  
-        thankYouMessageTerms.style.display = 'block';
-    });
+    if (acceptButtonTerms) {
+        acceptButtonTerms.addEventListener('click', function() {
+            localStorage.setItem('acceptedTerms', 'true');  
+            document.querySelector('.terms-section').style.display = 'none';  
+            thankYouMessageTerms.style.display = 'block';
+        });
+    }
 }
 
 const checkboxPrivacy = document.getElementById('privacy-checkbox');
 const acceptButtonPrivacy = document.getElementById('accept-button-privacy');
 const thankYouMessagePrivacy = document.getElementById('thank-you-message-privacy');
 
-if (localStorage.getItem('acceptedPrivacy')) {
+if (thankYouMessagePrivacy && localStorage.getItem('acceptedPrivacy')) {
     document.querySelector('.privacy-section').style.display = 'none';  
     thankYouMessagePrivacy.style.display = 'block';  
 } else {
-    checkboxPrivacy.addEventListener('change', function() {
-        acceptButtonPrivacy.disabled = !this.checked;
-    });
+    if (checkboxPrivacy) {
+        checkboxPrivacy.addEventListener('change', function() {
+            acceptButtonPrivacy.disabled = !this.checked;
+        });
+    }
 
-    acceptButtonPrivacy.addEventListener('click', function() {
-        localStorage.setItem('acceptedPrivacy', 'true');  
-        document.querySelector('.privacy-section').style.display = 'none';  
-        thankYouMessagePrivacy.style.display = 'block';
-    });
+    if (acceptButtonPrivacy) {
+        acceptButtonPrivacy.addEventListener('click', function() {
+            localStorage.setItem('acceptedPrivacy', 'true');  
+            document.querySelector('.privacy-section').style.display = 'none';  
+            thankYouMessagePrivacy.style.display = 'block';
+        });
+    }
 }
 
 const checkboxSecurity = document.getElementById('security-checkbox');
 const acceptButtonSecurity = document.getElementById('accept-button-security');
 const thankYouMessageSecurity = document.getElementById('thank-you-message-security');
 
-if (localStorage.getItem('acceptedSecurity')) {
+if (thankYouMessageSecurity && localStorage.getItem('acceptedSecurity')) {
     document.querySelector('.security-section').style.display = 'none';  
     thankYouMessageSecurity.style.display = 'block';  
 } else {
-    checkboxSecurity.addEventListener('change', function() {
-        acceptButtonSecurity.disabled = !this.checked;
-    });
+    if (checkboxSecurity) {
+        checkboxSecurity.addEventListener('change', function() {
+            acceptButtonSecurity.disabled = !this.checked;
+        });
+    }
 
-    acceptButtonSecurity.addEventListener('click', function() {
-        localStorage.setItem('acceptedSecurity', 'true');  
-        document.querySelector('.security-section').style.display = 'none';  
-        thankYouMessageSecurity.style.display = 'block';
-    });
-            
+    if (acceptButtonSecurity) {
+        acceptButtonSecurity.addEventListener('click', function() {
+            localStorage.setItem('acceptedSecurity', 'true');  
+            document.querySelector('.security-section').style.display = 'none';  
+            thankYouMessageSecurity.style.display = 'block';
+        });
+    }
 }
