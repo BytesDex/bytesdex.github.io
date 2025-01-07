99b661831b52717b8c94495dfc57bb22e13babc7
BytesDex <bytesdex.gt@gmail.com>
2025-01-07 00:17:38 -0600

    Delete resources/js/script-54.js

--- resources/js/script-54.js
+++
@@ -1,6 +0,0 @@
-function copyHTML(index) {
-  var htmlCode = document.getElementsByTagName('textarea')[index].value;
-  navigator.clipboard.writeText(htmlCode)
-    .then(() => alert("Código copiado al portapapeles"))
-    .catch(err => console.error('Error al copiar el código: ', err));
-}
